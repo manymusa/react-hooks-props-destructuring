233caa7d71870acf06958bf7254a9e024e8bc0ca
manymusa <adanalin113@gmail.com>
2022-09-22 15:45:23 -0400

    commit tree fa85849e061f625da04d78f89dddbbc0534a3482

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,38 @@
 import React from "react";
 
-function MovieCard({
-  title,
-  posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
-  genres,
-}) {
+
+// Destructoring with a default value
+// function MovieCard({
+//   title,
+//   posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
+//   genres,
+// }) {
+//   return (
+//     <div className="movie-card">
+//       <img src={posterSrc} alt={title} />
+//       <h2>{title}</h2>
+//       <small>{genres.join(", ")}</small>
+//     </div>
+//   );
+// }
+
+// export default MovieCard;
+
+//using prop to create more dynamic code
+
+// function MovieCard(props){
+//   return (
+//     <div className="movie-card">
+//       <img 
+//       src={props.posterSrc}
+//       alt = {props.title} />
+//       <h2>{props.title}</h2>
+//       <small>{props.genres.join(" , ")}</small>
+//     </div>
+//   )
+// }
+
+function MovieCard({title, posterSrc, genres}){
   return (
     <div className="movie-card">
       <img src={posterSrc} alt={title} />
@@ -14,4 +42,23 @@ function MovieCard({
   );
 }
 
-export default MovieCard;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default MovieCard
+
